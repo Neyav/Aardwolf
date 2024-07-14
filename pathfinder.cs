ffa8e3c564d19183c6886cbcd6b351c33584c095
Chris Laverdure <greghealburn@hotmail.com>
2024-07-13 18:30:04 -0600

    Switch from a brute force map sweep to a queue system for nodes. So much faster.

--- pathfinder.cs
+++ pathfinder.cs
@@ -5,15 +5,11 @@ namespace Aardwolf
     internal class pathfindingGraph
     {
         public List<int[][]> graphMap;
-        public List<int> floorMap;
-        public List<int> HeightCeiling;
-        public List<int> WidthCeiling;
-        public List<int> HeightFloor;
-        public List<int> WidthFloor;
-        public List<bool> floorCleared;
+        public List<int> floorMap;        
         public List<bool[][]> visited;
         public List<int[][]> travelDistance;
         public List<int[][]> travelDirection;
+        public Queue<pathfindingNode> pathfindingQueue;
         private int _Width;
         private int _Height;
 
@@ -50,7 +46,6 @@ namespace Aardwolf
 
             graphMap.Add(newFloor);
             floorMap.Add(baseFloor); // Keep track of the floor we came from.
-            floorCleared.Add(false); // Keep track of if the floor has been cleared or not.
 
             // Debug print out a floor was created and from where.
             Debug.WriteLine("Floor " + (graphMap.Count - 1) + " created from floor " + baseFloor);
@@ -95,14 +90,10 @@ namespace Aardwolf
             // Initialize the graph with a single floor.
             graphMap = new List<int[][]>();
             floorMap = new List<int>();
-            floorCleared = new List<bool>();
             visited = new List<bool[][]>();
             travelDistance = new List<int[][]>();
             travelDirection = new List<int[][]>();
-            HeightCeiling = new List<int>();
-            WidthCeiling = new List<int>();
-            HeightFloor = new List<int>();
-            WidthFloor = new List<int>();
+            pathfindingQueue = new Queue<pathfindingNode>();
 
             this.addFloor(-1);
         }
@@ -135,107 +126,13 @@ namespace Aardwolf
         {
             pathfindingNode smallestNode = new pathfindingNode(-1, -1, -1);
 
-            // Find the smallest travel distance node that hasn't been visited yet across all floors.
-            for (int i = 0; i < _graph.graphMap.Count; i++)
+            if (_graph.pathfindingQueue.Count == 0)
             {
-                if (_graph.floorCleared[i] == true)
-                {
-                    continue;
-                }
-                bool floorCleared = true; // Assume the floor is cleared until proven otherwise.
-
-                // If there is no heightfloor/heightceiling/etc for this floor, scan it quickly for nodes, and set the floor bounds.
-                if (_graph.HeightFloor.Count < i + 1)
-                {
-                    _graph.HeightFloor.Add(_mapdata.getMapHeight());
-                    _graph.WidthFloor.Add(_mapdata.getMapWidth());
-                    _graph.HeightCeiling.Add(0);
-                    _graph.WidthCeiling.Add(0);
-
-                    for (int j = 0; j < _mapdata.getMapHeight(); j++)
-                    {
-                        for (int k = 0; k < _mapdata.getMapWidth(); k++)
-                        {
-                            if (_graph.travelDistance[i][j][k] > -1)
-                            {
-                                if (j <= _graph.HeightFloor[i])
-                                {
-                                    _graph.HeightFloor[i] = j - 1;
-                                }
-                                if (j >= _graph.HeightCeiling[i])
-                                {
-                                    _graph.HeightCeiling[i] = j + 1;
-                                }
-                                if (k <= _graph.WidthFloor[i])
-                                {
-                                    _graph.WidthFloor[i] = k - 1;
-                                }
-                                if (k >= _graph.WidthCeiling[i])
-                                {
-                                    _graph.WidthCeiling[i] = k + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                // Debug out heightfloor/ceiling/etc
-                //Debug.WriteLine("Floor " + i + " HeightFloor: " + _graph.HeightFloor[i] + " HeightCeiling: " + _graph.HeightCeiling[i] + " WidthFloor: " + _graph.WidthFloor[i] + " WidthCeiling: " + _graph.WidthCeiling[i]);
-
-
-                for (int j = _graph.HeightFloor[i]; j <= _graph.HeightCeiling[i]; j++)
-                {
-                    for (int k = _graph.WidthFloor[i]; k <=_graph.WidthCeiling[i]; k++)
-                    {
-                        if (_graph.visited[i][j][k] == false && _graph.travelDistance[i][j][k] != -1)
-                        {
-                            floorCleared = false;
-
-                            // If this = the ceiling/floor bounds, we need to expand that bound.
-                            if (j == _graph.HeightFloor[i])
-                            {
-                                _graph.HeightFloor[i] = j - 1;
-                            }
-                            if (j == _graph.HeightCeiling[i])
-                            {
-                                _graph.HeightCeiling[i] = j + 1;
-                            }
-                            if (k == _graph.WidthFloor[i])
-                            {
-                                _graph.WidthFloor[i] = k - 1;
-                            }
-                            if (k == _graph.WidthCeiling[i])
-                            {
-                                _graph.WidthCeiling[i] = k + 1;
-                            }
-
-                            // if our smallest node is set to -1, then any valid node will be the smallest.
-                            if (smallestNode.height == -1)
-                            {
-                                smallestNode.height = j;
-                                smallestNode.width = k;
-                                smallestNode.floor = i;            
-                            }
-                            else
-                            {
-                                if (_graph.travelDistance[i][j][k] < _graph.travelDistance[smallestNode.floor][smallestNode.height][smallestNode.width])
-                                {
-                                    smallestNode.height = j;
-                                    smallestNode.width = k;
-                                    smallestNode.floor = i;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (floorCleared)
-                {
-                    _graph.floorCleared[i] = true;
-                    // We didn't find any unvisited nodes on this floor, consider it cleared and never look at it again.
-                    // Display to debug
-                    Debug.WriteLine("Floor " + i + " has been cleared.");
-                }
+                return smallestNode;
+            }
+            else
+            {
+                smallestNode = _graph.pathfindingQueue.Dequeue();
             }
 
             return smallestNode;
@@ -264,6 +161,7 @@ namespace Aardwolf
         {
             // Check to see if there's a pushwall in that direction.
             pathfindingNode updateNode = currentNode;
+            pathfindingNode pushWallBlock = currentNode;
             bool pushwallCanMove = false;
 
             switch (direction)
@@ -286,6 +184,7 @@ namespace Aardwolf
             {
                 pathfindingNode pushWallNode = updateNode;
                 pathfindingNode pushWallTest = updateNode;
+                pushWallBlock = updateNode;
 
                 // If we push it in this direction can it move, and if so would it be able to move one block or two blocks?
                 switch (direction)
@@ -340,15 +239,19 @@ namespace Aardwolf
                 {
                     // Debug text the pushwall location that lead to this new floor.
                     Debug.WriteLine("Pushwall found at: " + updateNode.height + " " + updateNode.width + " " + updateNode.floor);
+                    Debug.WriteLine("From: " + currentNode.height + " " + currentNode.width + " " + currentNode.floor);
 
                     // Clone a new floor and move us to that floor.
                     int newFloor = _graph.addFloor(currentNode.floor);
                     _graph.setGraphValue(newFloor, updateNode.height, updateNode.width, -1);
-                    _graph.setGraphValue(newFloor, currentNode.height, currentNode.width, 1);
+                    _graph.setGraphValue(newFloor, pushWallBlock.height, pushWallBlock.width, 1);
 
                     // Set the travel distance and to be the same on the new floor, but the direction to be 5, to indicate we're going down to the floor that spawned this floor.
                     _graph.travelDistance[newFloor][currentNode.height][currentNode.width] = _graph.travelDistance[currentNode.floor][currentNode.height][currentNode.width] + 5;
                     _graph.travelDirection[newFloor][currentNode.height][currentNode.width] = 5;
+
+                    // add this node to the queue
+                    _graph.pathfindingQueue.Enqueue(new pathfindingNode(currentNode.height, currentNode.width, newFloor));
 
                     return pushwallCanMove;
                 }
@@ -407,6 +310,9 @@ namespace Aardwolf
                 _graph.travelDistance[newFloor][updateNode.height][updateNode.width] = _graph.travelDistance[currentNode.floor][currentNode.height][currentNode.width] + 1;
                 _graph.travelDirection[newFloor][updateNode.height][updateNode.width] = 5;
 
+                // Add this node to the queue.
+                _graph.pathfindingQueue.Enqueue(new pathfindingNode(updateNode.height, updateNode.width, newFloor));
+
                 if (travelDistance == -10)
                 {
                     // Look for the gold door and set it to 2.
@@ -440,7 +346,7 @@ namespace Aardwolf
             {
 
                 // If the new node has a smaller travel distance than what we're going to update it to, don't update it.
-                if (_graph.travelDistance[updateNode.floor][updateNode.height][updateNode.width] != -1 && _graph.travelDistance[updateNode.floor][updateNode.height][updateNode.width] < _graph.travelDistance[currentNode.floor][currentNode.height][currentNode.width] + travelDistance)
+                if (_graph.travelDistance[updateNode.floor][updateNode.height][updateNode.width] != -1 && _graph.travelDistance[updateNode.floor][updateNode.height][updateNode.width] <= _graph.travelDistance[currentNode.floor][currentNode.height][currentNode.width] + travelDistance)
                 {
                     return;
                 }
@@ -448,6 +354,9 @@ namespace Aardwolf
                 // Update the travel distance of the new node along with the direction we traveled to get there.
                 _graph.travelDistance[updateNode.floor][updateNode.height][updateNode.width] = _graph.travelDistance[currentNode.floor][currentNode.height][currentNode.width] + travelDistance;
                 _graph.travelDirection[updateNode.floor][updateNode.height][updateNode.width] = direction;
+
+                // Add the new node to the queue.
+                _graph.pathfindingQueue.Enqueue(new pathfindingNode(updateNode.height, updateNode.width, updateNode.floor));
             }
         }
 
@@ -470,11 +379,19 @@ namespace Aardwolf
             // Set the first position to be explored with a travel distance of 0.
             _graph.travelDistance[0][firstNodeHeight][firstNodeWidth] = 0;
             // Travel direction of -1 is the start point. 1-4 are the directions of N, E, S, W. 4 + floor number is the travel floor.
-            _graph.travelDirection[0][firstNodeHeight][firstNodeWidth] = -1;            
+            _graph.travelDirection[0][firstNodeHeight][firstNodeWidth] = -1;
+
+            // Add this node to the queue.
+            _graph.pathfindingQueue.Enqueue(new pathfindingNode(firstNodeHeight, firstNodeWidth, 0));
 
             while (true)
             {
                 pathfindingNode currentNode = findsmallestunexploredNode();
+
+                // Print size of the queue to debug.
+                //Debug.WriteLine("Queue size: " + _graph.pathfindingQueue.Count);
+                // print node grabbed from it.
+                //Debug.WriteLine("Node grabbed: " + currentNode.height + " " + currentNode.width + " " + currentNode.floor);
 
                 if (currentNode.height == -1)
                 {

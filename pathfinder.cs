9f853b13d5752f9060549efd1ea28902cfdb453e
Chris Laverdure <greghealburn@hotmail.com>
2024-09-21 03:55:19 -0600

    Better node generation but not perfect.

--- pathfinder.cs
+++ pathfinder.cs
@@ -35,62 +35,66 @@ namespace Aardwolf
         private maphandler _mapdata;
         private List<pathNode> _nodes;
 
+        private bool tileBlocked(int heightPosition, int widthPosition)
+        {
+            if (_mapdata.getTileData(heightPosition, widthPosition) != 0)
+                return true;
+
+            if (_mapdata.isTileBlocked(heightPosition, widthPosition))
+                return true;
+
+            return false;
+        }
+
+        // Returns a unique code for the blocked status of the corners of a tile.
+        private int cornerBlocked(int heightPosition, int widthPosition)
+        {
+            int code = 0;
+            // Check NE corner.
+            if (tileBlocked(heightPosition - 1, widthPosition - 1))
+                code = code + 1;
+            // Check SE corner.
+            if (tileBlocked(heightPosition + 1, widthPosition - 1))
+                code = code + 10;
+            // Check SW corner.
+            if (tileBlocked(heightPosition + 1, widthPosition + 1))
+                code = code + 100;
+            // Check NW corner.
+            if (tileBlocked(heightPosition - 1, widthPosition + 1))
+                code = code + 1000;
+
+            return code;
+        }
         public bool tileNodeWorthy(int heightPosition, int widthPosition)
         {
-            // We're checking to see if a tile is node worthy. To make the check it first needs to be a floor tile.
-            if (_mapdata.getTileData(heightPosition, widthPosition) != 0)
+            int cornerBlockedcode = 0;
+
+            // If the tile is blocked it cannot be a node.
+            if (tileBlocked(heightPosition, widthPosition))
                 return false;
 
-            // We will run a sanitation check to ensure that the tile is not already a node.
+            // If the tile is already a node it cannot be a node.
             foreach (pathNode node in _nodes)
             {
                 if (node.heightPosition == heightPosition && node.widthPosition == widthPosition)
                     return false;
             }
 
-            // The tile needs to be blocked on at least one of it's NE/SE/SW/NW sides.
-            bool cornerBlocked = false;
-            if (_mapdata.getTileData(heightPosition - 1, widthPosition - 1) != 0)
-                cornerBlocked = true;
-            if (_mapdata.getTileData(heightPosition - 1, widthPosition + 1) != 0)
-                cornerBlocked = true;
-            if (_mapdata.getTileData(heightPosition + 1, widthPosition + 1) != 0)
-                cornerBlocked = true;
-            if (_mapdata.getTileData(heightPosition + 1, widthPosition - 1) != 0)
-                cornerBlocked = true;
+            // If the tile is not blocked on at least one of it's NE/SE/SW/NW sides it cannot be a node.
+            cornerBlockedcode = cornerBlocked(heightPosition, widthPosition);
 
-            // Now do the same check but for blocking objects, providing that the corner is not blocked.
-            if (!cornerBlocked)
-            {
-                if (_mapdata.isTileBlocked(heightPosition - 1, widthPosition - 1) == true)
-                    cornerBlocked = true;
-                if (_mapdata.isTileBlocked(heightPosition - 1, widthPosition + 1) == true)
-                    cornerBlocked = true;
-                if (_mapdata.isTileBlocked(heightPosition + 1, widthPosition + 1) == true)
-                    cornerBlocked = true;
-                if (_mapdata.isTileBlocked(heightPosition + 1, widthPosition - 1) == true)
-                    cornerBlocked = true;
-            }
-
-            if (!cornerBlocked)
+            if (cornerBlockedcode == 0)
                 return false;
 
-            // Now it also needs to blocked on A N/S side and a E/W side.
-            bool sideBlocked = false;
-            if (_mapdata.getTileData(heightPosition - 1, widthPosition) != 0 || _mapdata.isTileBlocked(heightPosition - 1, widthPosition))
-                sideBlocked = !sideBlocked;
-            if (_mapdata.getTileData(heightPosition + 1, widthPosition) != 0 || _mapdata.isTileBlocked(heightPosition + 1, widthPosition))
-                sideBlocked = !sideBlocked;
-
-            bool sideBlocked2 = false;
-
-            if (_mapdata.getTileData(heightPosition, widthPosition - 1) != 0 || _mapdata.isTileBlocked(heightPosition, widthPosition - 1))
-                sideBlocked2 = !sideBlocked2;
-            if (_mapdata.getTileData(heightPosition, widthPosition + 1) != 0 || _mapdata.isTileBlocked(heightPosition, widthPosition + 1))
-                sideBlocked2 = !sideBlocked2;
-
-            if (!sideBlocked || !sideBlocked2)
+            // If two tiles adjacent to this tile have the same cornerBlocked code, this tile cannot be a node.
+            // Check E/W
+            if (cornerBlocked(heightPosition, widthPosition - 1) == cornerBlockedcode && cornerBlocked(heightPosition, widthPosition + 1) == cornerBlockedcode)
                 return false;
+
+            // Check N/S
+            if (cornerBlocked(heightPosition - 1, widthPosition) == cornerBlockedcode && cornerBlocked(heightPosition + 1, widthPosition) == cornerBlockedcode)
+                return false;
+
 
             return true;
         }
